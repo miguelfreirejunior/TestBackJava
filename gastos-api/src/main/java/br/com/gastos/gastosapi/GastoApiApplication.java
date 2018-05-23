b455e05dc4d93d37b50771dddaeb38321dfff00f
Freire <miguel.freire@accenture.com>
2018-05-23 12:55:30 -0300

    salvando dados para transferencia de pc

--- gastos-api/src/main/java/br/com/gastos/gastosapi/GastoApiApplication.java
+++ gastos-api/src/main/java/br/com/gastos/gastosapi/GastoApiApplication.java
@@ -2,10 +2,12 @@ package br.com.gastos.gastosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableOAuth2Sso
 public class GastoApiApplication {
 
 	public static void main(String[] args) {
